c2321305801342f99e6e22475cd998efcd22c179
jhosuapp <jhosuapvll@gmail.com>
2025-10-29 13:52:44 -0500

    feat: clear calculator app

---
+++ 02-calculator-app/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const CalculatorApp = () => {
+    return (
+        <View>
+            <Text>Hola mundo</Text>
+        </View>
+    )
+}
+
+export default CalculatorApp;
