3e89755e0079a9db6d2c000e270393c04c72fb84
jhosuapp <jhosuapvll@gmail.com>
2025-11-05 08:08:11 -0500

    feat: init navigation app

---
+++ 03-navigation-app/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const App = () => {
+    return (
+        <View>
+            <Text>Hola mundo</Text>
+        </View>
+    )
+}
+
+export default App;
